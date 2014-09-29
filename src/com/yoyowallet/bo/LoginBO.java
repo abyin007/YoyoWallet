755b275dd7948adf50d8e88abf8c790e8beb41ff
Aby Sebastian <abyin007@users.noreply.github.com>
2014-09-29 21:08:34 +0530

    Following updates: 	1. Login using JDBC 	2. Cleanup of registering

---
+++ src/com/yoyowallet/bo/LoginBO.java
@@ -0,0 +1,22 @@
+package com.yoyowallet.bo;
+
+import com.yoyowallet.dao.LoginDAO;
+import com.yoyowallet.dto.common.Login;
+
+/**
+ * This class involves business logic for login operation.
+ * 
+ * @author Aby
+ * 
+ */
+public class LoginBO {
+
+	LoginDAO loginDAO = new LoginDAO();
+
+	public boolean loginUser(Login login) {
+		// TODO: Perform validation, also retrieve the entire userdetails
+		// instead of just a boolean value.
+		return loginDAO.loginUser(login);
+	}
+
+}
---
+++ src/com/yoyowallet/dao/LoginDAO.java
@@ -0,0 +1,63 @@
+package com.yoyowallet.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.yoyowallet.dto.common.Login;
+import com.yoyowallet.utils.DBConnection;
+
+/**
+ * This class performs the DB access for login operations.
+ * 
+ * @author Aby
+ * 
+ */
+public class LoginDAO {
+
+	DBConnection dBConnection = new DBConnection();
+	Connection connection = null;
+
+	public LoginDAO() {
+		try {
+			connection = dBConnection.getMySQLDataSource().getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public boolean loginUser(Login login) {
+		ResultSet resultSet = null;
+		String loginQuery = null;
+		boolean loginStatus = false;
+		if ("@".contains(login.getUserID())) {
+			loginQuery = "SELECT username FROM userdetails WHERE email=? AND password=?";
+		} else {
+			loginQuery = "SELECT username FROM userdetails WHERE username=? AND password=?";
+		}
+
+		if (connection != null) {
+			try {
+				PreparedStatement statement = connection
+						.prepareStatement(loginQuery);
+				statement.setString(1, login.getUserID());
+				statement.setString(2, login.getPassword());
+				resultSet = statement.executeQuery();
+				if (resultSet.next()) {
+					loginStatus = true;
+					System.out.println("Login Success.");
+				} else {
+					System.out.println("Login Failed.");
+				}
+				statement.close();
+				connection.close();
+			} catch (SQLException exception) {
+				exception.printStackTrace();
+				return false;
+			}
+		}
+		return loginStatus;
+	}
+}
